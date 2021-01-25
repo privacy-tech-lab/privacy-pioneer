ebccfbed64ce0eb2251efd0a1dbe943d02235da1
Owen Kaplan <owenmkaplan@gmail.com>
2021-01-24 22:00:50 -0500

    add http encryption check for sent requests

--- src/analysis/analyze.js
+++ src/analysis/analyze.js
@@ -5,7 +5,27 @@ analyze.js
 */
 
 function analyzeRequests(details) {
-  console.log(details)
+  console.log(details);
+  try {
+    if (details.url.split(':')[0] == 'http') {
+      console.log('unencrypted request sent!')
+    }
+  }
+  catch (error) { console.error(error)};
+}
+
+// function to check for https connection
+// guaranteed a valid url
+// log result to console for now
+function httpsCheck(url) {
+  if (url.split(':')[0] == 'http'){
+    console.log(url.concat(' is unencrypted!'));
+  }
+  else {
+    console.log(url.concat(' is encrypted!'));
+  }
 }
 
 export { analyzeRequests }
+
+export { httpsCheck }
