5cbf5164e6ae2dab796ca031e08d317b16f580c0
Rafael <rgoldstein01@wesleyan.edu>
2021-02-17 14:11:53 -0500

    removed unused folders

--- privacy-analysis-tool/browsermobproxy/exceptions.py
+++
@@ -1,2 +0,0 @@
-class ProxyServerError(Exception):
-    pass
--- privacy-analysis-tool/extension/src/dashboard/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("btn").addEventListener("click", () => {
-    const url = document.getElementById("input").value;
-    chrome.tabs.update({ url: url });
-  });
-});
