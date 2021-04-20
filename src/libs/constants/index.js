1464bc54e9486f34a0afe4244f4d420f1d30ed45
David Baraka <47195003+davebaraka@users.noreply.github.com>
2021-04-20 02:15:54 -0400

    add example privacy label structure #93

--- src/libs/constants/index.js
+++ src/libs/constants/index.js
@@ -1,3 +1,5 @@
 import _keywordTypes from './data/keyword-types.json'
+import _privacyLabels from "./data/privacy-labels.json"
 
 export const keywordTypes = _keywordTypes
+export const privacyLabels = _privacyLabels
