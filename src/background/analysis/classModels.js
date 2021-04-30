2d85015fa17582662cde0372674e78793747a2a4
David Baraka <47195003+davebaraka@users.noreply.github.com>
2021-04-30 14:48:53 -0400

    add privacy label model #93

--- src/background/analysis/classModels.js
+++ src/background/analysis/classModels.js
@@ -5,7 +5,7 @@ requestModel.js
 */
 
 export class Request {
-  constructor({id, details, requestHeaders, responseHeaders, requestBody, responseData, error}) {
+  constructor({ id, details, requestHeaders, responseHeaders, requestBody, responseData, error }) {
     this.id = id // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/filterResponseData#parameters
     this.requestHeaders = requestHeaders // https://developer.mozilla.org/en-US/docs/Glossary/Request_header
     this.responseHeaders = responseHeaders // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/HttpHeaders
@@ -19,29 +19,26 @@ export class Request {
 // class model for evidence type for when we find a keyword or URL
 // we want index as an array [start, finish]
 export class Evidence {
-  constructor({timestamp, permission, rootUrl, snippet, requestUrl, typ, index}) {
-    this.timestamp = timestamp;
-    this.permission = permission;
-    this.rootUrl = rootUrl;
-    this.snippet = snippet;
-    this.requestUrl = requestUrl;
-    this.typ = typ;
+  constructor({ timestamp, permission, rootUrl, snippet, requestUrl, typ, index }) {
+    this.timestamp = timestamp
+    this.permission = permission
+    this.rootUrl = rootUrl
+    this.snippet = snippet
+    this.requestUrl = requestUrl
+    this.typ = typ
     this.index = index === undefined ? -1 : index
   }
 }
 
-
-
-export const permissionEnum = Object.freeze( {
+export const permissionEnum = Object.freeze({
   Location: "location",
   PersonalData: "personalData",
   Fingerprinting: "Fingerprinting",
   Advertising: "Advertising",
   Content: "Content",
-} )
+})
 
-
-export const typeEnum = Object.freeze( { 
+export const typeEnum = Object.freeze({
   CoarseLocation: "coarse_location",
   TightLocation: "tight_location",
   City: "City",
@@ -53,8 +50,91 @@ export const typeEnum = Object.freeze( {
   Analytics: "Analytics",
   TrackingPixel: "TrackingPixel",
   CryptoMining: "CryptoMining",
-  // for now we change convention to be consistent with the .json file 
+  // for now we change convention to be consistent with the .json file
   Phone: "phone_number",
   Email: "email_address",
   Zipcode: "zip_code",
-} )
+})
+
+export const privacyLabels = Object.freeze({
+  location: {
+    displayName: "Location",
+    description: "",
+    types: {
+      coarseLocation: {
+        displayName: "Coarse Location",
+        description: "",
+      },
+      tightLocation: {
+        displayName: "Tight Location",
+        description: "",
+      },
+      zipCode: {
+        displayName: "Zip Code",
+        description: "",
+      },
+      ipAddress: {
+        displayName: "Ip Address",
+        description: "",
+      },
+      streetAddress: {
+        displayName: "Street Address",
+        description: "",
+      },
+      city: {
+        displayName: "City",
+        description: "",
+      },
+      state: {
+        displayName: "State",
+        description: "",
+      },
+      state: {
+        displayName: "State",
+        description: "",
+      },
+    },
+  },
+  personalData: {
+    displayName: "Personal Data",
+    description: "",
+    types: {
+      phoneNumber: {
+        displayName: "Phone Number",
+        description: "",
+      },
+      emailAddress: {
+        displayName: "Email Address",
+        description: "",
+      },
+      social: {
+        displayName: "Social",
+        description: "",
+      },
+      userKeyword: {
+        displayName: "Keyword",
+        description: "",
+      },
+    },
+  },
+  advertising: {
+    displayName: "Personal Data",
+    description: "",
+    types: {
+      trackingPixel: {
+        displayName: "Tracking Pixel",
+        description: "",
+      },
+    },
+  },
+  fingerprinting: {
+    displayName: "Fingerprinting",
+    description: "",
+    types: {},
+  },
+  content: {
+    displayName: "???",
+    description: "",
+    types: {},
+  },
+})
