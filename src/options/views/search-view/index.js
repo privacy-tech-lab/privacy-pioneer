5842df19ba128e1d6a56d5529c0c99996031e24a
Logan Brown <lrbrown826@gmail.com>
2021-06-16 10:56:51 -0400

    #170 Added back button to history section

--- src/options/views/search-view/index.js
+++ src/options/views/search-view/index.js
@@ -1,48 +1,68 @@
-import React, { useEffect, useState } from "react"
-import Scaffold from "../../components/scaffold"
-import { SInput, SInputContainer, STitle } from "./style"
-import { SContainer, SSubtitle } from "./style"
-import * as Icons from "../../../libs/icons"
-import WebsiteLabelList from "../../components/website-label-list"
-import { getWebsites } from "../../../libs/indexed-db"
+import React, { useEffect, useState } from "react";
+import Scaffold from "../../components/scaffold";
+import { SBackButton, SInput, SInputContainer, STitle, STop } from "./style";
+import { SContainer, SSubtitle } from "./style";
+import * as Icons from "../../../libs/icons";
+import WebsiteLabelList from "../../components/website-label-list";
+import { getWebsites } from "../../../libs/indexed-db";
+import { useHistory } from "react-router";
 
 /**
  * Search view allowing user to search from identified labels
  */
 const SearchView = () => {
-  const [allWebsites, setAllWebsites] = useState({})
-  const [filteredSites, setFilter] = useState({})
+  const [allWebsites, setAllWebsites] = useState({});
+  const [filteredSites, setFilter] = useState({});
+  const history = useHistory();
 
   /**
    * Filter websites based on user input string from text field
    */
   const filter = (keyString) => {
-    const filteredKeys = Object.keys(allWebsites).filter((k) => k.includes(keyString))
-    var filteredWebsites = {}
-    filteredKeys.forEach((websiteName) => (filteredWebsites[websiteName] = allWebsites[websiteName]))
-    setFilter(filteredWebsites)
-  }
+    const filteredKeys = Object.keys(allWebsites).filter((k) =>
+      k.includes(keyString)
+    );
+    var filteredWebsites = {};
+    filteredKeys.forEach(
+      (websiteName) =>
+        (filteredWebsites[websiteName] = allWebsites[websiteName])
+    );
+    setFilter(filteredWebsites);
+  };
 
   useEffect(() => {
     getWebsites().then((websites) => {
-      setAllWebsites(websites)
-      setFilter(websites)
-    })
-  }, [])
+      setAllWebsites(websites);
+      setFilter(websites);
+    });
+  }, []);
 
   return (
     <Scaffold>
       <SContainer>
-        <STitle>History</STitle>
-        <SSubtitle>See browsed webistes accessing and sharing your personal information</SSubtitle>
+        <STop>
+          <SBackButton
+            onClick={() => history.goBack()}
+            whileHover={{ scale: 1.2 }}
+          >
+            <Icons.Arrow size={18} />
+          </SBackButton>
+          <STitle>History</STitle>
+        </STop>
+        <SSubtitle>
+          See browsed webistes accessing and sharing your personal information
+        </SSubtitle>
         <SInputContainer>
           <Icons.Search size="24px" />
-          <SInput placeholder="Search" onChange={(e) => filter(e.target.value)} />
+          <SInput
+            placeholder="Search"
+            onChange={(e) => filter(e.target.value)}
+          />
         </SInputContainer>
         <WebsiteLabelList websites={filteredSites} />
       </SContainer>
     </Scaffold>
-  )
-}
+  );
+};
 
-export default SearchView
+export default SearchView;
