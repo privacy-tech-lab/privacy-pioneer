300bfb6109e87d06e12b736f8dd4257fe019337d
Logan Brown <lrbrown826@gmail.com>
2021-06-29 10:18:56 -0400

    comments and adjustments to search

--- src/options/views/home-view/index.js
+++ src/options/views/home-view/index.js
@@ -3,7 +3,6 @@ import { useHistory } from "react-router";
 import {
   getWebsites,
   getLabels,
-  getWebsiteLabels,
   getAllWebsiteLabels,
 } from "../../../libs/indexed-db";
 import Scaffold from "../../components/scaffold";
