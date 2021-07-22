c7e314762ee6a4960b3d15ff438c53b274943aba
Owen Kaplan <owenmkaplan@gmail.com>
2021-07-22 14:29:19 -0400

    #203 documentation

--- src/exportData/initiateDownload.js
+++ src/exportData/initiateDownload.js
@@ -10,7 +10,7 @@ function blobToURL(blob){
 
     // creates a URL with the data from the blob passed
     const dataURL = URL.createObjectURL(blob)
-    
+
     return dataURL
 }
 
@@ -22,7 +22,8 @@ function blobToURL(blob){
  * The CSV file is much smaller: it only includes the part of snippets that we have indexes for (like in the popup)
  * 
  * The JSON file dumps everything we have
- * @param {string} exportDataType From exportTypeEnum: The type of data the user wants their data to be formatted
+ * @param {string} exportDataType From exportTypeEnum: The type of data the user wants their data to be formatted. TSV or JSON.
+ * @returns {void} Nothing. Initiates the download.
  */
 async function initiateDownload(exportDataType = exportTypeEnum.TSV) {
     
