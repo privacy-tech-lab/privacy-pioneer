20c13bb2d42b0e65de56b98e828fd43345259fdc
Logan Brown <lrbrown826@gmail.com>
2021-08-09 10:08:41 -0400

    added package and file

---
+++ src/libs/tour/index.js
@@ -0,0 +1,40 @@
+import Joyride from "react-joyride"
+
+const steps = [
+  {
+    target: "",
+    content: "",
+  },
+  {
+    target: "",
+    content: "",
+  },
+  ,
+  {
+    target: "",
+    content: "",
+  },
+  {
+    target: "",
+    content: "",
+  },
+  {
+    target: "",
+    content: "",
+  },
+  {
+    target: "",
+    content: "",
+  },
+  ,
+  {
+    target: "",
+    content: "",
+  },
+  {
+    target: "",
+    content: "",
+  },
+]
+
+const Tour = () => {}
