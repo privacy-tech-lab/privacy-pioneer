43341c6e112db439116bed0174edb9e671591f86
danielgoldelman <daniel.goldelman@gmail.com>
2021-09-05 10:40:10 -0400

    check against time

--- src/background/analysis/analyze.js
+++ src/background/analysis/analyze.js
@@ -145,14 +145,19 @@ function resolveBuffer(id, data) {
 async function analyze(request, userData) {
 
   const allEvidence = getAllEvidenceForRequest(request, userData);
-  const allCookieEvidence = getAllEvidenceForCookies(await browser.cookies.getAll({url: request.reqUrl}), request.rootUrl, request.reqUrl, userData)
-
+  const currentUrl = request.rootUrl
+  const currentTime = Date.now()
+  var allCookieEvidence = []
+  if (currentTime - window.sessionStorage.getItem(currentUrl) < 60000) {
+    allCookieEvidence = getAllEvidenceForCookies(await browser.cookies.getAll({url: request.reqUrl}), request.rootUrl, request.reqUrl, userData)
+  }
+  
   if (allCookieEvidence.length != 0) {
     allCookieEvidence.forEach(cookieEv => {
       allEvidence.push(cookieEv)
     })
   }
-  
+
   // if we found evidence for the request
   if (allEvidence.length != 0) {
     const rootUrl = request.rootUrl
