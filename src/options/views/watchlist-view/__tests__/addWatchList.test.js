3c942230132f023921cafe041a8ae5182d9aceca
Logan Brown <lrbrown826@gmail.com>
2022-09-15 23:46:33 -0400

    implement jest and single test

---
+++ src/options/views/watchlist-view/__tests__/addWatchList.test.js
@@ -0,0 +1,9 @@
+import { watchlistKeyval } from "../../../../libs/indexed-db/openDB";
+import { saveKeyword } from "../../../../libs/indexed-db/updateWatchlist";
+
+test('add general item to the watchlist', async () => {
+    const res = await saveKeyword("This is a Test", "userKeyword", '12345')
+    expect(res).toBeTruthy()
+    const keyword = await watchlistKeyval.get('12345')
+    expect(keyword).not.toBeUndefined();
+});
