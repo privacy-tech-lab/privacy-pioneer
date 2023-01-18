4f83a23d3a92a834df7dacb28a59630366033601
JustinCasler <97986810+JustinCasler@users.noreply.github.com>
2023-01-18 18:42:06 -0500

    Adds extra tests to existing suites #454

--- src/tests/analysis-functionality/userData.test.js
+++ src/tests/analysis-functionality/userData.test.js
@@ -1,6 +1,6 @@
 import { apiIPToken } from "../../libs/holdAPI.js";
 import { getLocationData } from "../../background/analysis/buildUserData/getLocationData.js";
-import { getWatchlistDict, hashUserDictValues} from "../../background/analysis/buildUserData/structureUserData.js";
+import { getWatchlistDict, hashUserDictValues, createKeywordObj} from "../../background/analysis/buildUserData/structureUserData.js";
 import analysisData from "../mock-data/analysisData.json";
 
   test("test getLocationData", async () => {
@@ -15,4 +15,14 @@ import analysisData from "../mock-data/analysisData.json";
     const keywords = analysisData.networkKeywords
     expect(hashUserDictValues(keywords)).toBe(keywords)
     
+  });
+
+  test("test general hashUserDictValues", async () => {
+    const general = analysisData.general
+    expect(hashUserDictValues(general).watchlist.general[0].keywordHash).toBeTruthy()
+  });
+
+  test("test createKeywordObj", async () => {
+    expect(createKeywordObj("123\\D?45\\D?678\\D?910", "ipAdress").keywordHash).toBeTruthy()
+    expect(createKeywordObj("/testRegion/", "Region", "222222222").keywordHash).toBe("222222222")
   });
