9bdb2eb29e094437a555477c88effaafdd1824bf
JustinCasler <97986810+JustinCasler@users.noreply.github.com>
2023-01-18 18:42:06 -0500

    Adds extra tests to existing suites

--- src/tests/analysis-functionality/addtoEvidence.test.js
+++ src/tests/analysis-functionality/addtoEvidence.test.js
@@ -1,10 +1,14 @@
 import { addToEvidenceStore } from "../../background/analysis/interactDB/addEvidence.js"
 import { loadModel, useModel } from "../../background/analysis/interactDB/ml/jsrun.js"
 
-test("test ml Model", async () => {
 /*
-  await loadModel();
-  const test = await useModel("region ___ \Federal States of Micronesia\",FL:\"Florida\",GA:\"Georgia\",GU:\"Guam\",HI:\"Hawaii\",ID:\"Idaho\",IL:\"Illinois\",IN:\"Indiana\",IA:\"Iowa\",KS:\"Kansas\",KY:\"Kentucky\",LA:\"Louisiana\",ME:\"Maine\",MH:\"Marshall Islands\",MD:\"Maryland\",MA:\"<TARGET_REGION>\",MI:\"Michigan\",MN:\"Minnesota\",MS:\"Mississippi\",MO:\"Missouri\",MT:\"Montana\",NE:\"Nebraska\",NV:\"Nevada\",NH:\"New Hampshire\",NJ:\"New Jersey\",NM:\"New Mexico\",NY:\"New York\",NC:\"North Carolina\",ND:\"North Dakota\",MP:\"Northern Maria")
-  await expect(test).resolves.toBeTruthy()
-*/
+jest.setTimeout(40000)
+test("test load Model", async () => {
+   await loadModel();
 });
+
+test("test ml Model", async () => {
+  const test = await useModel("region ___ \Federal States of Micronesia\",FL:\"Florida\",GA:\"Georgia\",GU:\"Guam\",HI:\"Hawaii\"")`
+  await expect(test).resolves.toBeTruthy()
+});
+*/
