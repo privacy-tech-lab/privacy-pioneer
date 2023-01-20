f5f069b911dc0e99ad589d48498c3fcb2241e128
Logan Brown <lrbrown826@gmail.com>
2023-01-20 17:42:57 -0500

    repositioned watchlist tool tip

--- src/options/views/watchlist-view/components/list-item/style.js
+++ src/options/views/watchlist-view/components/list-item/style.js
@@ -23,22 +23,14 @@ export const SItem = styled.div`
     }
   }
 `;
-export const SButtonText = styled.div`
+
+export const SSection = styled.div`
   margin-bottom: 16px;
   display: flex;
   flex-direction: row;
-  > * {
-    :first-child {
-      width: 50%;
-    }
-  }
-  > * {
-    :last-child {
-      display: flex;
-      justify-content: space-between;
-      flex-grow: 1;
-    }
-  }
+`;
+export const SButtonText = styled.text`
+  display: flex;
 `;
 
 export const SKeyword = styled.div``;
